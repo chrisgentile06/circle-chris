b8bd56e461da05002dc2b330d6d4f21061f8e2b9
chris <gentf697@newschool.edu>
2024-11-18 15:28:33 -0500

    classwork

---
+++ button.js
@@ -0,0 +1,11 @@
+console.log("file is working");
+
+let backgroundButton = document.getElementById("button");
+
+console.log(backgroundButton)
+
+backgroundButton.addEventListener("click", function(event) {
+    console.log("clicked");
+    document.body.classList.toggle("button-active");
+});
+
